97ee89e26b3e4ee7a562213f7e5864e08328cba7
LilyWS <stranger1212ss23@gmail.com>
2021-09-01 11:28:50 -0400

    finished type multipliers

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ looks like: ?p1=charizard&p2=pikachu&loc=
 var parameters = new URLSearchParams(window.location.search);
 
 var weatherAPIKey = 'e93a9a6062496e0d1483164567d29081';
+//TODO: account for spaces by adding hyphens (tapu lele)
+//CONVERT TO LOWER
 var pokemon1= (parameters.get("p1")) ? parameters.get("p1") : "squirtle";
 var pokemon2= (parameters.get("p2")) ? parameters.get("p2") : "bulbasaur";
 var pokemon = []; //will contain objects representing pokemon 1 and 2
@@ -38,14 +40,23 @@ function getPokemon(url, url2) {
     });
 }
 
-function getType(url, pokeIndex) {
+function getType(url, url2) {
     fetch(url)
     .then(function (response) {
         return response.json();
       })
     .then(function (data) {
-        console.log(data);
-        pokemon[pokeIndex].typeData = data;
+        pokemon[0].typeData = data;
+        loadTypes(0)
+        return
+    });
+    fetch(url2)
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function (data) {
+        pokemon[1].typeData = data;
+        loadTypes(1)
         return(data);
     });
 }
@@ -54,6 +65,8 @@ function createPokemon(data, pokeIndex){ //pokeIndex is which index of the pokem
     //we have to do a second api call to get the specification of their type and how it relates to other types
     //let typeUrl = `https://pokeapi.co/api/v2/type/${}/`;
     //let typeData = getType()
+
+    //TODO: catch edge cases where the pokemon has no back sprite or front sprite
     pokemon[pokeIndex] = {
         name: data.name,
         spriteBack: data.sprites.back_default,
@@ -68,7 +81,13 @@ function createPokemon(data, pokeIndex){ //pokeIndex is which index of the pokem
         spd: data.stats[5].base_stat,
     }
     if (pokemon[0] && pokemon[1]){ //check if both pokemon are loaded
+        getType(`https://pokeapi.co/api/v2/type/${pokemon[0].type}/`, `https://pokeapi.co/api/v2/type/${pokemon[1].type}/`);
         renderPokemon()
+    }
+}
+
+function loadTypes(pokeIndex) { //this function exists so that the game will not try to run before the type api call completes
+    if (pokemon[0].typeData && pokemon[1].typeData){ //check if both pokemon are loaded
         initBattle()
     }
 }
@@ -86,25 +105,35 @@ the damgage given will follow the following formula:
 function initBattle(){ //set up the battle 
     let p1 = pokemon[0];
     let p2 = pokemon[1];
-    getType(`https://pokeapi.co/api/v2/type/${p1.type}/`, 0);
-    getType(`https://pokeapi.co/api/v2/type/${p2.type}/`, 1);
     order = (p1.spd>p2.spd) ? [p1, p2] : [p2, p1];
-    //determine what the pokemon will attack and defend with
+    //determine what stat the pokemon will attack and defend with
     p1.using = (p1.sAtk > p1.atk) ? 'sAtk' : 'atk';
     p2.defWith = (p1.using=='sAtk') ? p2.sDef : p2.def;
     p2.using = (p2.sAtk > p2.atk) ? 'sAtk' : 'atk';
     p1.defWith = (p2.using=='sAtk') ? p1.sDef : p1.def;
     //determine how type will multiply their attacks
-    //p1.multiplier = 
+
+    p1.atkMult = getAtkMult(p1.typeData.damage_relations, p2.type);
+    p2.atkMult = getAtkMult(p2.typeData.damage_relations, p1.type);
     //battleTimer = setInterval(battleStep, 250);
+}
+
+function getAtkMult(dmgRelations, targetType){
+    if (dmgRelations.double_damage_to.some(e => e.name === targetType)) {
+        return(2); 
+    }else if (dmgRelations.half_damage_to.some(e => e.name === targetType)) {
+        return(.5);
+    }else if (dmgRelations.no_damage_to.some(e => e.name === targetType)) {
+        return(.25);
+    }
 }
 
 function battleStep() { //function to process one step of the battle (a turn for both players)
     for(let i=0;i<order.length;i++){
         let p1 = order[i]; //in this context p1 is the currently attacking pokemon and p2 is the defending one
         let p2 = (i) ? order[0]:order[1];
-        let dmgVal = Math.round(((p1[p1.using]/p2.defWith) * (Math.random()*.15 + .85) * 1)*10) / 10 //1 will become type multiplier later. we also round damage to one decimal place
-        p2.cHp = Math.round((p2.cHp - dmgVal) *10)/10; //we round to tenths place because javascript sucks at floating point numbers
+        let dmgVal = Math.round(((p1[p1.using]/p2.defWith) * (Math.random()*.15 + .85) * p1.atkMult)*10) / 10 //we round damage to one decimal place
+        p2.cHp = Math.round((p2.cHp - dmgVal) *10)/10; //we round health to tenths place because javascript sucks at floating point numbers
         
         if(p2.cHp < .1){
             console.log(`${p2.name} is hit for ${dmgVal} and faints!`);
